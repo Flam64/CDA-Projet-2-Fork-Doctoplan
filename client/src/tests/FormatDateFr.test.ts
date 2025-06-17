793506e1dc7a91d43db7cafbb650047ecbc866b4
Flam64 <celio.mozes.rocha@gmail.com>
2025-06-17 16:51:45 +0200

    feat(test-formatDate): unit test of the formatdate function

--- client/src/tests/FormatDateFr.test.ts
+++ client/src/tests/FormatDateFr.test.ts
@@ -1,8 +1,34 @@
-import { test, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import { formatDate } from '@/utils/formatDateFr';
 
-test('formatDate should format the date correctly', () => {
-  const inputDate = '2025-06-17';
-  const result = formatDate(inputDate);
-  expect(result).toBe('17/06/2025');
+describe('formatDate', () => {
+  it('the conversion from yyyy-mm-dd to dd-mm-yyyy format is correct', () => {
+    //const inputDate = '2025-06-17';
+    const result = formatDate('2025-06-17');
+    expect(result).toBe('17/06/2025');
+  });
+
+  it('should return an empty string if the date is empty', () => {
+    const emptyDate = '';
+    const result = formatDate(emptyDate);
+    expect(result).toBe('');
+  });
+
+  it('should manage the entry properly with a single digit of days and months', () => {
+    const singleDigitMonthsAndDays = '2025/1/7';
+    const result = formatDate(singleDigitMonthsAndDays);
+    expect(result).toBe('07/01/2025');
+  });
+
+  it('should properly manage a bissextile year date', () => {
+    const bissextileYear = '2028/02/29';
+    const result = formatDate(bissextileYear);
+    expect(result).toBe('29/02/2028');
+  });
+
+  it('should correctly return the current date', () => {
+    const dateNow = new Date().toLocaleDateString();
+    const result = formatDate(dateNow);
+    expect(result).toBe('17/06/2025');
+  });
 });
