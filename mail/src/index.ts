9c148d4a41051c2bd60344ccb53c9a1036438888
abdifatah <abdifatah4@msn.com>
2025-06-19 16:14:34 +0200

    feat(server/user): send created user confirmation mail

--- mail/src/index.ts
+++ mail/src/index.ts
@@ -18,7 +18,7 @@ app.get('/mail', (req, res) => {
   res.send('Welcome to the server (email sending management).');
 });
 
-const port = process.env.SERVER_PORT_MAIL || '3030';
+const port = process.env.SERVER_PORT_MAIL;
 
 // 📋 checking that the SMTP server is working properly
 transporter.verify((error, success) => {
